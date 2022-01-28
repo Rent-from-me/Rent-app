8a87bb2cf749057723d5dc219d9859cd067aebcc
cayto <aidarous1143@gmail.com>
2022-01-28 21:13:33 +0300

    fixed some style errors

--- src/component/Rent/Rentinfo/RentItemInfo.js
+++ src/component/Rent/Rentinfo/RentItemInfo.js
@@ -1,15 +1,19 @@
-import React, { Profiler } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
+import "./RentItemInfo.css";
 import RenterInfo from "./RenterInfo";
-import "./RentItemInfo.css"
+
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+
 const RentItemInfo = (props) => {
   const params = useParams();
   const selectedItem = props.data.find(
     (selected) => selected.id === +params.id
   );
+  const { id, title, img, reviewer, review } = selectedItem;
 
-  const { id, title, img,reviewer,review} = selectedItem;
-  
   return (
     <div className="item__info">
       <section className="item__info--header ">
@@ -23,53 +27,52 @@ const RentItemInfo = (props) => {
           <h4 className="item_info-loction">Location of Host</h4>
         </div>
       </section>
-      <section>
-        <div className="item__info-imgs d-flex mt-5 align-items-center">
-          <div className="item__info-img item__info-img--main">
-            <img
-              src={require(`../../../asset/camer${id}.jpg`)}
-              alt="img"
-              className="item__info_img"
-            />
-          </div>
-          <div className="item-info-img--subs d-flex flex-wrap">
-            <div className="item__info-img item-info-img--sub ms-3">
-              {" "}
-              <img
-                src={require(`../../../asset/camer2.jpg`)}
-                alt="img"
-                className="item__info_img"
-              />
-            </div>
-            <div className="item__info-img item-info-img--sub ms-3">
-              {" "}
-              <img
-                src={require(`../../../asset/camer3.jpg`)}
-                alt="img"
-                className="item__info_img"
-              />
-            </div>
-            <div className="item__info-img item-info-img--sub ms-3">
-              {" "}
+      <Box sx={12} className=" mt-5 mx-auto d-flex" gap={1}>
+        <Grid xs={6.6} className="item__info-img--main grid">
+          <img
+            src={require(`../../../asset/camer${id}.jpg`)}
+            alt="img"
+            className="item__info_img"
+          />
+        </Grid>
+        <Grid xs={6} className="item-info-img--subs grid d-flex flex-wrap">
+          <Grid xs={12} className="d-flex" gap={1}>
+            <Grid xs={6} className="item-info-img--sub grid2">
               <img
                 src={require(`../../../asset/camer4.jpg`)}
                 alt="img"
                 className="item__info_img"
               />
-            </div>
-            <div className="item__info-img item-info-img--sub ms-3">
-              {" "}
+            </Grid>
+            <Grid xs={6} className="item-info-img--sub grid2">
               <img
                 src={require(`../../../asset/camer5.jpg`)}
                 alt="img"
                 className="item__info_img"
               />
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="mt-5 item__rent-data">
-        <RenterInfo data={selectedItem} />
+            </Grid>
+          </Grid>
+          <Grid xs={12} className="d-flex" gap={1}>
+            <Grid xs={6} className="item-info-img--sub grid2">
+              <img
+                src={require(`../../../asset/camer3.jpg`)}
+                alt="img"
+                className="item__info_img"
+              />
+            </Grid>
+            <Grid xs={6} className="item-info-img--sub grid2">
+              <img
+                src={require(`../../../asset/camer2.jpg`)}
+                alt="img"
+                className="item__info_img"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <section>
+        <RenterInfo data ={selectedItem}/>
       </section>
     </div>
   );
